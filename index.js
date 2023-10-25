954fff78bb2fdc2d33d5008891c595ff70e2b9ff
Jiří Ryšavý <rysavji22@sps-prosek.cz>
2023-10-25 08:40:24 +0200

    kid & parent buttons

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+$("#kid-button").click(function(){
+    $("#parent-box").hide();
+    $("#kid-box").show();
+});
 
+$("#parent-button").click(function(){
+    $("#kid-box").hide();
+    $("#parent-box").show();
+});
